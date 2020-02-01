3599da344986d78572be94d94aff2d51a071bcda
Mason Davis <1mason.davis@gmail.com>
2020-02-01 10:27:58 -1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ["queen","queen","king","king"];
+let cardOne =  cards[0];
+let cardTwo = cards[1];
+let cardsInPlay = []; 
+cardsInPlay.push("queen"); cardsInPlay.push("king");
+if (cardsInPlay.length === 2) {
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+   	alert("You found a match!");
+   }
+   else {
+   	alert("Sorry, try again.");
+   }
+} 
 console.log("User flipped " + cardOne + " " + cardTwo);
